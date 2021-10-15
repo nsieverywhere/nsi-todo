7ef77a45b9ac553c3a7030d09d2386407a302a59
nsieverywhere <nsikansimon@gmail.com>
2021-10-15 08:48:24 +0100

    Still working on it

--- src/Pages/home/home.jsx
+++ src/Pages/home/home.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
-import {Card} from 'Components/card';
+import React from 'react';
+import Entry from 'Components/entry';
 import Home from './home.module.css';
+import Cards from 'Components/card'
+
+const CardItem = [];
 
 function home() {
+
+    const onAdd = (entry) =>{
+        CardItem.push(entry);
+        console.log(CardItem);
+    }
+
+
     return (
         <div>
-           <Card />
+            <Entry onAdd={onAdd} />
+            <div className={Home.cardcontainer}>
+               <Cards title="my first todo" cardcontent="nsi is the best " />
+                <Cards title="my first todo" cardcontent="nsi is the best" />                                                                                        
+            </div>
         </div>
     )
 }
